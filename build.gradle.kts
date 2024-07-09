4f1b3ed38d36ed9d53c657d19f9bc8f9fd03de20
Micah Baker <micahb923@gmail.com>
2024-07-09 13:14:34 -0700

    The previous menu architecture - to be discussed during our meeting tonight

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,17 +5,12 @@ plugins {
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
-val exposedVersion = "0.52.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    implementation("com.h2database:h2:2.2.224")
-    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
-    implementation("org.jetbrains.exposed", "exposed-dao", exposedVersion)
-    implementation("org.jetbrains.exposed", "exposed-jdbc", exposedVersion)
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
     testImplementation("io.kotest:kotest-property:5.9.1")
@@ -26,7 +21,7 @@ tasks.test {
 }
 
 application {
-    mainClass = "anttracker.MainKt"
+    mainClass = "MainKt"
 }
 
 kotlin {
